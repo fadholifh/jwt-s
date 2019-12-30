dad99e1b50ba3a0cfb64839ec2f6b9a885894415
fadholifh <fadholifh14@gmail.com>
2019-12-30 23:00:05 +0700

    first for validation15

--- routes/auth.js
+++ routes/auth.js
@@ -26,17 +26,17 @@ router.post("/register", async (req, res) => {
   
 //   res.send(validation);
   
-  const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-  });
-  try {
-      const savedUser = await user.save();
-      res.send(savedUser);
-  } catch{
-      res.status(400).send(err);
-  }
+//   const user = new User({
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: req.body.password
+//   });
+//   try {
+//       const savedUser = await user.save();
+//       res.send(savedUser);
+//   } catch{
+//       res.status(400).send(err);
+//   }
 });
 
 router.get("/registers", (req, res) => {
