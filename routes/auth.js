48dbe1932e8e247501d69d0d21744e199c6d6d0f
fadholifh <fadholifh14@gmail.com>
2020-01-03 21:08:13 +0700

    add jsonwebtoken to auth done

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
-const router = require("express").Router();
-const User = require("../model/User");
-const bcrypt = require("bcryptjs");
-const { registerValidation, loginValidation } = require("../validation");
+const router = require('express').Router();
+const User = require('../model/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation } = require('../validation');
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   //Validate The Data Before
   const { error } = registerValidation(req.body);
 
@@ -39,6 +40,10 @@ router.post('/login', async (req, res) => {
     //password correct
     const validPass = await bcrypt.compare(req.body.password, user.password)
     if (!validPass) return res.status(400).send('Invalid password');
+
+    //create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 
     res.send('Loged in');
 });
